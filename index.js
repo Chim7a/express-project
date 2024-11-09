c571c0ba91d861a906aa6adc2a8d409c06878e2c
chim7a <chimalawrence11@gmail.com>
2024-11-09 01:44:04 +0100

    nav bar

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from "express";
+const app = express();
+const port = 4001;
+
+// Pug template engine
+app.set("view engine", "pug");
+app.set("views", "./views");
+
+// Built-in middleware for static files like css
+app.use(express.static("public"));
+
+app.use((req, res, next) => {
+  const day = new Date().getDay();
+  const hour = new Date().getHours();
+
+  //   console.log(day);
+  //   console.log(hour);
+  //   console.log(req.url);
+
+  //   if ((hour >= 9) & (hour <= 17) & (day >= 6)) {
+  //     return next();
+  //   }
+
+  if (day >= 6) {
+    return next();
+  }
+
+  res.status(300).send("We have closed for the day");
+});
+
+app.get("/", (req, res) => {
+  res.render("content");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
